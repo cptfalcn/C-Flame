aa5883ecb47f7085014be22589ba3626a68dc37b
cptfalcn <54957544+cptfalcn@users.noreply.github.com>
2021-06-10 15:15:13 -0700

    Getting TChem up and running
    First compile of a TChem fucntion

--- Flame/ZeroD/Kapili.cpp
+++ Flame/ZeroD/Kapili.cpp
@@ -5,15 +5,6 @@
  * written using objects. 
  * 
  * -------------------------------------------------------------------------------------------
- * 
- * This is implementaion of Burgers equation for Epic,
- * 
- *                                    u_t + u u_x = Nu u_xx
- * 
- * with Dirichlet boundary conditions and initial condition is given by
- * 
- *                               u = (sin(3 pi x))^3 (1-x)^(3/2)
- * 
  * -------------------------------------------------------------------------------------------
  * 
  * This is serial implementation. 
@@ -27,13 +18,13 @@
 #include <nvector/nvector_serial.h>
 #include <sundials/sundials_nvector.h>
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
 // Number of points in the grid
 #define NEQ 3
 #define EPS 1e-2
 N_Vector InitialConditions();
-void Lu(N_Vector, N_Vector);
-void Mu(realtype, N_Vector, N_Vector);
 int RHS(realtype, N_Vector, N_Vector, void *);
 int Jtv(N_Vector, N_Vector, realtype, N_Vector, N_Vector , void *, N_Vector);
 int CheckStep(realtype, realtype);
@@ -42,111 +33,195 @@ using namespace std;
 
 int main()
 {
-    // Declaration of start and finish time
-    //static const realtype InitTime = 0.0;
-    static const realtype FinalTime = 10.0;
-    static const int NumBands = 3;
-    static const realtype StepSize=1e-1;
-	//check if we can make the stepsize
-	static const int Steps=CheckStep(FinalTime, StepSize);
+	//====================
+	// Declarations
+	//====================
+	static const realtype FinalTime = 10.0;
+	static const int NumBands = 3;
+	realtype StepSize=1e-1/(1*2*2*2*2*2*2*2);
+	CheckStep(FinalTime, StepSize);
+	int ProgressDots=0; //From 0 to 100; only care about percentage
+	int StepCount = 0;
+	realtype PercentDone=0;
 	void *userData=nullptr;
 	realtype TNow=0;
 	realtype TNext=0;
-    // Setup the initial condition (and solution) y
-    N_Vector y = N_VNew_Serial(NEQ);
+	N_Vector y = N_VNew_Serial(NEQ); //The data y
 	N_VScale(1., InitialConditions(), y);
 	realtype *data = NV_DATA_S(y);
+	bool LastStepAdjusted = 0;
+
 	//=====================================
-	//This block is testing, remove later
-	//actually, we need some of this.
+	//This block is for testing.
+	//Reactivate at your own risk,
+	//The entire block is a mess.
 	//=====================================
 	//N_Vector Is = N_VNew_Serial(NEQ);
 	//N_Vector Jv = N_VNew_Serial(NEQ);
 	//N_Vector rhs= N_VNew_Serial(NEQ);
-	N_Vector y0 = N_VNew_Serial(NEQ);
+	//N_Vector y0 = N_VNew_Serial(NEQ);
+        //N_Vector y0 = N_VNew_Serial(NEQ);
+	//N_VScale(1., InitialConditions(), y0);
+        //realtype *Y0= NV_DATA_S(y0);
 	//realtype *IS = NV_DATA_S(Is);
 	//realtype *JV = NV_DATA_S(Jv);
 	//realtype *RHSy=NV_DATA_S(rhs);
-	realtype *Y0= NV_DATA_S(y0);
-	N_VScale(1., InitialConditions(), y0);
+	//realtype *Y0= NV_DATA_S(y0);
+	//N_VScale(1., InitialConditions(), y0);
 	//for (int i=0; i<NEQ; i++){
 	//	IS[i]=1.0;}
 	//RHS(0, y, rhs, userData);
 	//Jtv(Is, Jv, 1, y , rhs, userData, Is);
 	//cout << "=================Initial Data===================\n";
-	cout << "=========================y======================\n";
-	cout << "y[0]=" << data[0] <<"\t\t y[1]=" << data[1] << "\t\t y[2]=" << data[2] << endl;
+	//cout << "=========================y======================\n";
+	//cout << "y[0]=" << data[0] <<"\t\t y[1]=" << data[1] << "\t\t y[2]=" << data[2] << endl;
 	//cout << "======================rhs========================\n";
 	//cout << "rhs[0]=" << RHSy[0] << "\t\t rhs[1]=" << RHSy[1] << "\t\t rhs[2]="<<RHSy[2] <<endl;
 	//cout << "========================JtV=====================\n";
 	//cout <<  "Jtv[0] =" <<JV[0] <<"\t\t Jtv[1]="<< JV[1] <<"\t\t Jtv[2]=" << JV[2]  <<endl;
-	//end test 
+	//end test
 
-    // Create the integrator
-    // In this case we use EpiRK5C, for other integrators just use their name like we did 
-    const int MaxKrylovIters = 500;
-    Epi2_KIOPS *integrator = new Epi2_KIOPS(RHS,
+
+	//===================================================
+	// Create the integrator
+	// In this case we use EpiRK5C, for other integrators just use their name.
+    	//===================================================
+	const int MaxKrylovIters = 500;
+    	Epi2_KIOPS *integrator = new Epi2_KIOPS(RHS,
                                       Jtv,
                                       userData,
                                       MaxKrylovIters,
                                       y,
                                       NEQ);
 
-    // Set run parameters
-    //const realtype StepSize = RCONST(4e-5);
-    const realtype KrylovTol = RCONST(1.0e-14);//1e-14
-    int startingBasisSizes[] = {3, 3};
-    
-    // Run the integrator loop
-	for (int i=0; i<Steps; i++){
-		TNow= i*StepSize;
-		TNext=(i+1)*StepSize;
-		//cout<< "Completed step " << i+1 << endl;
-		//cout<< "Stepped from " <<TNow << " to " <<TNext <<endl;
+	//========================
+	// Set integrator parameters
+	//========================
+	const realtype KrylovTol = RCONST(1.0e-14);//1e-14
+	int startingBasisSizes[] = {3, 3};
+
+
+
+	//=================================
+    	// Run the time integrator loop
+	//=================================
+	cout<<"Integrator progress:[";
+	while(TNext<FinalTime)
+	{
+		TNow= StepCount*StepSize;
+		TNext=(StepCount+1)*StepSize;
+		if(TNext>FinalTime&&(FinalTime != TNow))
+		{
+			StepSize=FinalTime-TNow;
+			LastStepAdjusted=1;
+		}
+		//Integrate
 		integrator->Integrate(
         	StepSize,TNow, TNext, NumBands, y,
 		KrylovTol, startingBasisSizes);
 		//If the data goes negative, which is not physical, correct it.
 		//Removing this loop will cause stalls.
-		for (int j=0; j<NEQ; j++){
-			if(data[j]<0){
+		for (int j=0; j<NEQ; j++)
+		{
+			if(data[j]<0)
+			{
 				data[j]=0;
 			}
 		}
-
+		//Track the progress
+		PercentDone=floor(TNext/FinalTime*100);
+		for (int k=0; k<PercentDone-ProgressDots;k++)
+		{
+			cout<<":";
+			cout.flush();
+		}
+		ProgressDots=PercentDone;
+		StepCount++;
 	}
+	TNow=TNext;
+	cout << "]100%\n\n";
 
 
 
+	//=====================================================
+	//Need to figure out why this  doesn't work as intended
+	//=====================================================
     	//IntegratorStats *integratorStats = integrator->Integrate(
 	//StepSize,InitTime, FinalTime, NumBands, y, KrylovTol, startingBasisSizes);
-    	// Print the statistics
+    	//Print the statistics
     	//printf("Run stats:\n");
     	//integratorStats->PrintStats();
 
 
 
-
-    printf("\n\n");
+	//=======================================
+	//Console Output
+	//=======================================
 	cout<<"===========================Data========================\n";
 	cout <<"y=" << data[0] << "\t\t z=" << data[1] <<"\t\t Temp=" << data[2]<<endl;
-	cout <<"Sum of y=" <<data[0]+ data [1] + data [2]<<endl;
-	cout <<"Error in sum=" << data[0]+data[1]+data[2]-Y0[0]-Y0[1]-Y0[2]<<endl;
-    // Clean up the integrator
-    delete integrator;
+	if (LastStepAdjusted == 1)
+	{
+		cout<<"We altered the last time step to not go over the final time\n";
+	}
+	cout << "Simulation ended at time: " << TNow << endl;
+	cout << "Steps taken: " << StepCount <<endl;
+	//cout <<"Sum of y=" <<data[0]+ data [1] + data [2]<<"\t\t";
+	//cout <<"Error in sum=" << data[0]+data[1]+data[2]-Y0[0]-Y0[1]-Y0[2]<<endl;
+	cout <<"===================Printing data to file==============\n";
+
+
+	//=======================================
+	//Data file output
+	//=======================================
+	ofstream myfile("OutPutData.txt", std::ios_base::app);
+	myfile << setprecision(17) << fixed << data[0] << "\t\t" << data[1] << "\t\t";
+	myfile << data[2] << "\t\t" <<StepSize <<endl;
+	myfile.close();
+	cout << "======================Simulation complete=============\n";
+
+   	 // Clean up the integrator
+    	delete integrator;
+
+
 
 }
 
 
 
-int CheckStep(realtype FinalTime, realtype StepSize){
-        if(fmod(FinalTime,1/StepSize) ==0){
+/*
+//=========================================================
+//  //====  ==   ==  ==     ==    _.==_    ========  _===_
+//  ||      ||   ||  ||\\   ||  ./     \\     ||    //   \\
+//  ||====  ||   ||  || \\  ||  ||            ||    \\___
+//  ||      ||   ||  ||  \\ ||	~\            ||         \\
+//  ||      \\___//  ||   \\||   ~=___-//     ||    \\___//
+//=========================================================
+*/
+
+
+//===========================================
+//Check the number of steps
+//FinalTime
+//StepSize
+//===========================================
+int CheckStep(realtype FinalTime, realtype StepSize)
+{
+	cout<<"======================Steps=========================\n";
+	cout<<"Checking the proposed number of steps...\n";
+	cout<<std::setprecision(17);
+	cout<<FinalTime/StepSize << " steps proposed...";
+
+        if(floor( FinalTime/StepSize ) == FinalTime/StepSize )
+	{
                 static const int Steps= FinalTime/StepSize;
-                cout << "We will take " <<Steps << " steps.\n";
+                cout << " accepted...\n";
 		return Steps;
-        }else{
+        }else if (abs(round(FinalTime/StepSize))-FinalTime/StepSize <1e-6 ){
+		static const int Steps= round (FinalTime/StepSize);
+		cout << Steps << " steps has been approximated...\n";
+		return Steps;
+	}else{
                 cout<<"Cannot perform non-integer number of steps!!\n";
-                cout<<"fmod(FinalTime, StepSize)="<<fmod(FinalTime,1/StepSize)<<endl;
                 exit(1);
         }
 }
