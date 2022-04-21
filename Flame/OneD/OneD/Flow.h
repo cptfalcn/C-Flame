8e81b6fe9b910e9f0c0a03152dd81d43a5279515
cptfalcn <54957544+cptfalcn@users.noreply.github.com>
2022-04-21 11:58:19 -0700

    Github reformat
    Restructured the files in backup

--- Flame/OneD/OneD/Flow.h
+++
@@ -1,5 +0,0 @@
-#include "Derivatives.h"
-
-void StaggerV(realtype *, realtype *, int);
-void Advection(realtype *, realtype *, realtype *, int);
-void Diffusion(realtype *, realtype *, realtype, realtype, int);
